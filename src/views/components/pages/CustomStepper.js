6eb0d9d9a22e1ef0df0a1c57ddf440b1b61310df
roshanwebee <roshanjajoriya1994@gmail.com>
2024-04-26 19:30:00 +0530

    working on the changes

--- src/views/components/pages/CustomStepper.js
+++ src/views/components/pages/CustomStepper.js
@@ -8,14 +8,32 @@ import BarChart from './Barchart';
 
 export default function CustomStepper() {
     const theme = useTheme();
-    const StepsWrap = styled(Box)(({ theme }) => ({
+    const StepMain = styled(Box)(({ theme }) => ({
         position: 'relative',
         width: '100%',
         height: '350px',
         display: 'flex'
     }));
+    const StepsWrap = styled(Box)(({ theme }) => ({
+        position: 'relative',
+        width: 'calc(100% - 50px)',
+        height: '350px',
+        margin: 'auto'
+    }));
     const Steps = styled(Button)(({ theme }) => ({
         position: 'absolute',
+        minWidth: 'inherit',
+        width: '35px',
+        height: '35px',
+        borderRadius: '50%',
+        padding: 0,
+        '&:hover': {
+            '.tooltipStep' : {
+                opacity: '1',
+                visibility: 'visible',
+                transform: 'translate(3px, 0)'
+            }
+        },
         '&.active': {
 			'.circle': {
                 backgroundColor: theme.palette.primary.main,
@@ -28,37 +46,20 @@ export default function CustomStepper() {
 		}
     }));
     const Circle = styled(Box)(({ theme }) => ({
-        width: '50px',
-        height: '50px',
-        display: 'block',
         backgroundColor: theme.palette.primary.extraLight,
         borderColor: theme.palette.primary.light,
         color: theme.palette.primary.main,
-        borderRadius: '50%',
+        width: '100%',
+        height: '100%',
         display: 'flex',
         alignItems: 'center',
         justifyContent: 'center',
         fontWeight: '500',
         textTransform: 'uppercase',
         letterSpacing: '0.5px',
+        borderRadius: '50%',
         zIndex: '99',
         position: 'relative'
-    }));
-    const Text = styled(Typography)(({ theme }) => ({
-        color: theme.palette.primary.main,
-        backgroundColor: 'white',
-        border: '1px solid',
-        borderColor: theme.palette.primary.main,
-        fontSize: '11px',
-        lineHeight: '100%',
-        fontWeight: '600',
-        textTransform: 'uppercase',
-        letterSpacing: '0.5px',
-        position: 'relative',
-        zIndex: 99,
-        borderRadius: '30px',
-        marginTop: '-1px',
-        padding: '2px'
     }));
     const LineWrapper = styled(Box)(({ theme }) => ({    
         width: '40%',    
@@ -68,6 +69,27 @@ export default function CustomStepper() {
         width: '100%',
         borderColor: theme.palette.primary.extraLight,
         borderWidth: '2px'
+    }));
+    const TooltipStep = styled(Box)(({ theme }) => ({
+        width: '150px',
+        backgroundColor: theme.palette.primary.main,
+        color: 'white',
+        padding: '20px',
+        borderRadius: '7px',
+        textAlign: 'center',
+        position: 'absolute',
+        transform: 'translate(3px, 10px)',
+        transition: 'all 0.4s ease',
+        opacity: '0',
+        visibility: 'hidden'
+    }));
+    const Tooltipline = styled(Box)(({ theme }) => ({
+        position: 'relative',
+        display: 'flex',
+        width: '35px',
+        height: '4px',
+        transform: 'rotate(90deg)',
+        backgroundColor: theme.palette.primary.main
     }));
     const handleStepClick = (stepNumber) => {
         if (stepNumber === 9) {
@@ -95,82 +117,83 @@ export default function CustomStepper() {
                     <Box style={{ marginTop: '-70px', marginBottom: '-30px', marginRight: '-25px' }} ><BarChart color={theme.palette.success.main} percentage={30} chartWidth="170" chartHeight="170" chartLableFonrSize="16px"  /></Box>
                 </Grid>
             </Grid>
-            <StepsWrap mt={4}>
-                <Tooltip arrow title={'Findings Shared – Awaiting Contract'}>
+            <StepMain mt={4}>
+                <StepsWrap>
                     <Steps className="active">
-                        <Box>
-                            <Circle className="circle">1</Circle>
-                        </Box>
+                        <Circle className="circle">1</Circle>
+                        <TooltipStep className="tooltipStep" style={{ top: '-85px' }}>
+                            <Typography variant="body2">Findings Shared – Awaiting Contract</Typography>
+                            <Tooltipline style={{ top: '35px', left: '34px' }}></Tooltipline>
+                        </TooltipStep>
                     </Steps>
-                </Tooltip>
-                <Tooltip title={'Hello Bro 2'}>
                     <Steps style={{ left: '0', right: '0', margin: 'auto' }}>
-                        <Box>
-                            <Circle>2</Circle>
-                        </Box>
+                        <Circle>2</Circle>
+                        <TooltipStep className="tooltipStep" style={{ top: '-85px' }}>
+                            <Typography variant="body2">Disputes Raised – Submitted</Typography>
+                            <Tooltipline style={{ top: '35px', left: '34px' }}></Tooltipline>
+                        </TooltipStep>    
                     </Steps>
-                </Tooltip>
-                <Tooltip title={'Hello Bro 3'}>
                     <Steps style={{ right: '0' }}>
-                        <Box>
-                            <Circle>3</Circle>
-                        </Box>
+                        <Circle>3</Circle>
+                        <TooltipStep className="tooltipStep" style={{ top: '-85px' }}>
+                            <Typography variant="body2">Disputes Raised – Submitted</Typography>
+                            <Tooltipline style={{ top: '35px', left: '34px' }}></Tooltipline>
+                        </TooltipStep>
                     </Steps>
-                </Tooltip>
-                <Tooltip title={'Hello Bro 4'}>
                     <Steps style={{ right: '0', top: '40%' }}>
-                        <Box>
-                            <Circle>4</Circle>
-                        </Box>
+                        <Circle>4</Circle>
+                        <TooltipStep className="tooltipStep" style={{ top: '-85px' }}>
+                            <Typography variant="body2">Disputes Raised – Submitted</Typography>
+                            <Tooltipline style={{ top: '35px', left: '34px' }}></Tooltipline>
+                        </TooltipStep>    
                     </Steps>
-                </Tooltip>
-                <Tooltip title={'Hello Bro 5'}>
                     <Steps style={{ left: '0', right: '0', top: '40%', margin: 'auto' }}>
-                        <Box>
-                            <Circle>5</Circle>
-                        </Box>
+                        <Circle>5</Circle>
+                        <TooltipStep className="tooltipStep" style={{ top: '-85px' }}>
+                            <Typography variant="body2">Disputes Raised – Submitted</Typography>
+                            <Tooltipline style={{ top: '35px', left: '34px' }}></Tooltipline>
+                        </TooltipStep>    
                     </Steps>
-                </Tooltip>
-                <Tooltip title={'Hello Bro 6'}>
                     <Steps style={{ left: '0', top: '40%' }}>
-                        <Box>
-                            <Circle>6</Circle>
-                        </Box>
+                        <Circle>6</Circle>
+                        <TooltipStep className="tooltipStep" style={{ top: '-85px' }}>
+                            <Typography variant="body2">Disputes Raised – Submitted</Typography>
+                            <Tooltipline style={{ top: '35px', left: '34px' }}></Tooltipline>
+                        </TooltipStep>    
                     </Steps>
-                </Tooltip>
-                <Tooltip title={'Hello Bro 7'}>
                     <Steps style={{ bottom: '0' }}>
-                        <Box>
-                            <Circle>7</Circle>
-                        </Box>
+                        <Circle>7</Circle>
+                        <TooltipStep className="tooltipStep" style={{ top: '-85px' }}>
+                            <Typography variant="body2">Disputes Raised – Submitted</Typography>
+                            <Tooltipline style={{ top: '35px', left: '34px' }}></Tooltipline>
+                        </TooltipStep>    
                     </Steps>
-                </Tooltip>
-                <Tooltip title={'Hello Bro 8'}>
                     <Steps style={{ left: '0', right: '0', bottom: '0', margin: 'auto' }}>
-                        <Box>
-                            <Circle>8</Circle>
-                        </Box>
+                        <Circle>8</Circle>
+                        <TooltipStep className="tooltipStep" style={{ top: '-85px' }}>
+                            <Typography variant="body2">Disputes Raised – Submitted</Typography>
+                            <Tooltipline style={{ top: '35px', left: '34px' }}></Tooltipline>
+                        </TooltipStep>
                     </Steps>
-                </Tooltip>
-                <Tooltip title={'Hello Bro 9'}>
                     <Steps style={{ right: '0', bottom: '0' }}>
-                        <Box>
-                            <Circle style={{ width: '70px', height: '70px', backgroundColor: 'white', borderRadisu: '0' }}><img width="100" src={SettledImg} alt='Settled' onClick={() => handleStepClick(9)}
-                             /></Circle>
-                        </Box>
+                        <Circle style={{ width: '70px', height: '70px', backgroundColor: 'white', borderRadisu: '0' }}><img width="100" src={SettledImg} alt='Settled' onClick={() => handleStepClick(9)}/></Circle>
+                        <TooltipStep className="tooltipStep" style={{ top: '-85px' }}>
+                            <Typography variant="body2">Disputes Raised – Submitted</Typography>
+                            <Tooltipline style={{ top: '35px', left: '34px' }}></Tooltipline>
+                        </TooltipStep>
                     </Steps>
-                </Tooltip>
-                <Box>
-                    <LineWrapper className="active" style={{ left: '0', top: '30px', transform: 'translateX(45px)'}}><Line></Line></LineWrapper>
-                    <LineWrapper style={{ right: '0', top: '30px',  transform: 'translateX(-45px)'}}><Line></Line></LineWrapper>
-                    <LineWrapper style={{ right: '0', top: '27%', transform: 'translate(40px) rotate(90deg)'}}><Line></Line></LineWrapper>
-                    <LineWrapper style={{ left: '0', top: '48%', transform: 'translateX(45px)'}}><Line></Line></LineWrapper>
-                    <LineWrapper style={{ right: '0', top: '48%', transform: 'translateX(-45px)'}}><Line></Line></LineWrapper>
-                    <LineWrapper style={{ left: '13px', top: '67%', transform: 'translate(-43px) rotate(90deg)'}}><Line></Line></LineWrapper>
-                    <LineWrapper style={{ left: '0', bottom: '30px', transform: 'translateX(45px)'}}><Line></Line></LineWrapper>
-                    <LineWrapper style={{ right: '0', bottom: '30px', transform: 'translateX(-45px)'}}><Line></Line></LineWrapper>
-                </Box>
-            </StepsWrap>
+                    <Box>
+                        <LineWrapper className="active" style={{ left: '0', top: '15px', transform: 'translateX(20px)'}}><Line></Line></LineWrapper>
+                        <LineWrapper style={{ right: '0', top: '15px',  transform: 'translateX(-20px)'}}><Line></Line></LineWrapper>
+                        <LineWrapper style={{ right: '0', top: '25%', transform: 'translate(22px) rotate(90deg)'}}><Line></Line></LineWrapper>
+                        <LineWrapper style={{ left: '0', top: '48%', transform: 'translateX(45px)'}}><Line></Line></LineWrapper>
+                        <LineWrapper style={{ right: '0', top: '48%', transform: 'translateX(-45px)'}}><Line></Line></LineWrapper>
+                        <LineWrapper style={{ left: '13px', top: '67%', transform: 'translate(-43px) rotate(90deg)'}}><Line></Line></LineWrapper>
+                        <LineWrapper style={{ left: '0', bottom: '30px', transform: 'translateX(45px)'}}><Line></Line></LineWrapper>
+                        <LineWrapper style={{ right: '0', bottom: '30px', transform: 'translateX(-45px)'}}><Line></Line></LineWrapper>
+                    </Box>
+                </StepsWrap>
+            </StepMain>
         </Box>
     )
 }
