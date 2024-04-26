c5f2be49d51adcc7bcaedc6756e298a69078ea53
Chetan Singh <chetan.singh@webeesocial.com>
2024-04-26 22:55:46 +0530

    added datagrid end table editable in Shortage Claim

--- src/views/components/pages/ShortageTable.js
+++ src/views/components/pages/ShortageTable.js
@@ -8,6 +8,8 @@ import {
     TableRow, styled, useTheme
 } from '@mui/material';
 import DashboardCard from '../../../components/shared/DashboardCard';
+import { DataGrid } from '@mui/x-data-grid';
+import { render } from 'react-dom';
 
 const TableHeadStyled = styled(TableHead)(({ theme }) => ({
     backgroundColor: theme.palette.primary.main,
@@ -28,6 +30,7 @@ const ShortageTable = () => {
     const formatNumber = (number) => new Intl.NumberFormat().format(number);
     const shortagetble = [
         {
+            id:1,
            Settlement: "Shortage Claim Finding",
            Active: ["$ 36,230.20"],
            Archieved: ["$ 36,230.20"],
@@ -35,6 +38,7 @@ const ShortageTable = () => {
         
         },
         {
+            id:2,
             Settlement: "Audit Period",
             Active: ["11/24/23"],
             Archieved: ["9/04/22"],
@@ -42,6 +46,7 @@ const ShortageTable = () => {
          
          },
          {
+            id:3,
             Settlement: "Audit Period",
             Active:["11/24/23"],
             Archieved: ["11/24/23"],
@@ -50,12 +55,14 @@ const ShortageTable = () => {
          
          },
          {
+            id:4,
             Settlement: "Case ID and Creation Date",
             Active: ['1423658', '11/24/23'],
             Archieved: ['1423658', '11/24/23'],
          
          },
          {
+            id:5,
             Settlement: "Open Balance",
             Active:["$ 36,230.20"],
             Archieved: ["$ 36,230.20"],
@@ -63,31 +70,35 @@ const ShortageTable = () => {
          },
         
           {
+            id:6,
             Settlement: "Confirmation Date",
             Active:["06/16/23"],
             Archieved: ["09/09/23"],
          
          },
          {
+            id:7,
              Settlement: "Settlement Offer",
              Active: ['$ 36,230.20', '08/04/23'],
              Archieved: ['$ 36,230.20', '08/04/23'],
           
           },
           {
+            id:8,
             Settlement: "Counter Offer",
             Active: ['$ 36,230.20', '08/04/23'],
             Archieved: ['$ 36,230.20', '08/04/23'],
          
          },
          {
+            id:9,
             Settlement: "Accepted Offer",
             Active: ['$ 36,230.20', '08/04/23'],
             Archieved: ['$ 36,230.20', '08/04/23'],
          
          },
           {
-            
+            id:10,
             Settlement: "Settlement Amount",
             Active:["$ 36,230.20"],
             Archieved: ["$ 36,230.20"],
@@ -97,74 +108,56 @@ const ShortageTable = () => {
      
     ];
 
+
+    const columns = [
+        {
+            field: 'Settlement',
+            headerName: 'Settlement',
+            width: 220,
+            renderCell: (params) => {
+                return (
+                    <Typography variant="subtitle2" fontWeight={600} >
+                        {params.value}
+                    </Typography>
+                );
+            }
+        },
+        {
+            field: 'Active',
+            editable: true,
+            headerName: 'Activity Cases($)',
+            width: 220,
+            renderCell: (params) => {
+                return (
+                    <Typography variant="subtitle2" fontWeight={600} style={{ color: params.row.color }}>
+                        { params.value[0] }
+                        { params.value[1] ? <Box>{params.value[1]}</Box> : null}
+                    </Typography>
+                );
+            }
+        },
+        {
+            field: 'Archieved',
+            editable:true,
+            headerName: 'Archived',
+            width: 220,
+            renderCell: (params) => {
+                return (
+                    <Typography variant="subtitle2" fontWeight={600} style={{ color: params.row.color }}>
+                        { params.value[0] }
+                        { params.value[1] ? <Box>{params.value[1]}</Box> : null}
+                    </Typography>
+                );
+            }
+        }
+    ];
+
     return (
         <DashboardCard title={<Typography variant='h4'> Shortage Claim Finding </Typography>}>
-            <Box sx={{ overflow: 'auto', width: { xs: '280px', sm: 'auto' } }}>
-                <Table aria-label="simple table" sx={{ whiteSpace: "nowrap", mt: 2 }}>
-                    <TableHeadStyled theme={theme}>
-                        <TableRow>
-                            <TableCell>
-                                <TableTypography variant="subtitle2" fontWeight={600}>
-                                </TableTypography>
-                            </TableCell>
-                            <TableCell>
-                                <TableTypography variant="subtitle2" fontWeight={600}>
-                                    Activity Cases($)
-                                </TableTypography>
-                            </TableCell>
-                            <TableCell>
-                                <TableTypography variant="subtitle2" fontWeight={600}>
-                                    Archived
-                                </TableTypography>
-                            </TableCell>
-                        </TableRow>
-                    </TableHeadStyled>
-                    <TableBody>
-                        {shortagetble.map((data, index) => (
-                            <TableRow theme={theme} key={index}>
-                                <TableCellStyled index={index}>
-                                   
-                                        <Typography
-                                            sx={{
-                                                fontSize: "15px",
-                                                fontWeight: "500",
-                                            }}
-                                        >
-                                            {data.Settlement}
-                                        </Typography>
-
-                                </TableCellStyled>
-                                <TableCell>
-                                    <Typography variant="subtitle2" fontWeight={600} style={{ color: data.color }}>
-                                       { 
-                                        data.Active.map((d,i)=>{
-                                            return (
-                                                <Typography key={i} variant="subtitle2" fontWeight={600} style={{ color: data.color }}>
-                                                    {typeof d === 'number' ? formatNumber(d) : d}
-                                                </Typography>
-                                            );
-                                        })
-                                    }
-                                    </Typography>
-                                </TableCell>
-                                <TableCell>
-                                    <Typography variant="subtitle2" fontWeight={600}>
-                                        {
-                                        data.Archieved.map((d,i)=>{
-                                            return (
-                                                <Typography key={i} variant="subtitle2" fontWeight={600} style={{ color: data.color }}>
-                                                    {typeof d === 'number' ? formatNumber(d) : d}
-                                                </Typography>
-                                            );
-                                        })
-                                    }
-                                    </Typography>
-                                </TableCell>
-                            </TableRow>
-                        ))}
-                    </TableBody>
-                </Table>
-            </Box>
+            <DataGrid
+                    rows={shortagetble}
+                    columns={columns}
+                />
         </DashboardCard>
     );
 };
