96e6dea191dc8ac078f5290ab168a4672be3bf0f
Himanshu Sharma <55379950+hsharma123@users.noreply.github.com>
2024-05-01 18:59:07 +0530

    all changes are done

--- src/views/excesscoopbilling/components/WinRate.js
+++ src/views/excesscoopbilling/components/WinRate.js
@@ -23,9 +23,12 @@ const WinRate = () => {
             offsetX: 0,
             offsetY: 0,
         },
+        legend: {
+            show: false,
+        },
         plotOptions: {
             bar: {
-                barHeight: '85%',
+                barHeight: '80%',
                 distributed: true,
                 horizontal: true,
                 borderRadius: 3,
@@ -47,6 +50,7 @@ const WinRate = () => {
                 colors: yAxisLabelColors,
                 fontSize: '14px',
                 fontWeight: '500',
+                textAlign: 'left'
 
             },
             formatter: function (val, opt) {
@@ -83,6 +87,9 @@ const WinRate = () => {
                     return val + '%'; 
                 },
             },
+            dataLabels: {
+                 show: true,
+            },
             axisBorder: {
                 show: true
             },
@@ -91,12 +98,15 @@ const WinRate = () => {
         yaxis: {
             labels: {
                 show: true,
-                offsetY: -14,
+                offsetY: 0,
                 style: {
                     fontSize: '14px',
                     colors: yAxisLabelColors,
                     fontWeight: '500',
-                    textAnchor: 'left',
+                    textAnchor: 'start',
+                },
+                axisBorder: {
+                    show: false,
                 },
                 maxWidth: 'auto',
             },
@@ -116,7 +126,6 @@ const WinRate = () => {
     return (
         <DashboardCard title={
             <Typography variant='h4' sx={{ color: theme.palette.text.dark }}>Win Rate</Typography>}>
-            <Divider />
             <Chart
                 options={optionsWinRate}
                 series={seriesWinRate}
